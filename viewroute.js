b1ffb78f3abe4c32805edefc95c0d0162201106a
Chase <1chasemcd@gmail.com>
2019-01-02 16:26:09 -0800

    Update viewroute scroll

--- viewroute.js
+++ viewroute.js
@@ -1,23 +1,21 @@
 window.onload = function() {
   var canvas = document.getElementById("viewcanvas");
-  if (window.innerWidth * (943/700) > window.innerHeight - 120 - 151) {
-    canvas.width = (window.innerHeight - 120 - 151) * (700/943);
-    canvas.height = window.innerHeight - 120 - 151;
-
-  } else {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerWidth * (943/700);
-  }
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerWidth * (943/700);
   var ctx = canvas.getContext("2d");
   var img = document.getElementById("img");
   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-  var ypos = 135 + canvas.height
-
-  if (ypos > window.innerHeight - 120) {
-    document.getElementById("inroute").style.top = window.innerHeight - 120 + "px";
-
-  } else {
-    document.getElementById("inroute").style.top = 135 + canvas.height + "px";
+  var pullup = document.getElementById("pullup");
+  var inroute = document.getElementById("inroute");
+  var stickyY = inroute.offsetTop - window.innerHeight;
+  window.onscroll = function() {
+    if (window.pageYOffset > stickyY) {
+      pullup.classList.add("pullupmove");
+      pullup.classList.remove("pullupfixed");
+    } else {
+      pullup.classList.remove("pullupmove");
+      pullup.classList.add("pullupfixed");
+    }
   }
 }
