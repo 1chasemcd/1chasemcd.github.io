0257bca0ac3f0387c18ba28f57f32108d68910fb
Chase <1chasemcd@gmail.com>
2019-01-06 12:30:10 -0800

    Updates

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+window.addEventListener('resize', function(event){setup()});
+window.addEventListener("load", function() {setup()}, false);
+
+function setup() {
+  var topNav = document.getElementById("topnav");
+  var content = document.getElementById("content");
+
+  content.style.marginTop = topNav.offsetHeight + 16 + "px";
+}
+
 function openRoute() {
   location.href='viewroute.html';
 }
--- viewroute.js
+++ viewroute.js
@@ -1,16 +1,28 @@
-window.onload = function() {
-  // Setup canvas
+window.addEventListener('resize', function(event){setup()});
+window.addEventListener("load", function() {setup()}, false);
+
+function setup() {
+  //Locate HTML elements
+  var topNav = document.getElementById("topnav");
   var canvas = document.getElementById("viewcanvas");
+  var img = document.getElementById("img");
+  var pullup = document.getElementById("pullup");
+  var inroute = document.getElementById("inroute");
+  var lowDiv = document.getElementById("bottom");
+
+  // Setup canvas
+  canvas.style.marginTop = topNav.offsetHeight + "px";
   canvas.width = window.innerWidth;
   canvas.height = window.innerWidth * (943/700);
   var ctx = canvas.getContext("2d");
-  var img = document.getElementById("img");
   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
   // Code for sticky bottom tab
-  var pullup = document.getElementById("pullup");
-  var inroute = document.getElementById("inroute");
   var stickyY = inroute.offsetTop - window.innerHeight;
+  if (window.innerHeight > canvas.height + topNav.offsetHeight){
+    pullup.classList.add("pullupmove");
+    pullup.classList.remove("pullupfixed");
+  }
   window.onscroll = function() {
     if (window.pageYOffset > stickyY) {
       pullup.classList.add("pullupmove");
@@ -19,6 +31,14 @@ window.onload = function() {
       pullup.classList.remove("pullupmove");
       pullup.classList.add("pullupfixed");
     }
+  }
+
+// Code for bottom spacefiller div
+  if (window.innerHeight > canvas.height + topNav.offsetHeight){
+    lowDiv.style.height = window.innerHeight -
+    (inroute.offsetTop + inroute.offsetHeight) + "px";
+  } else {
+    lowDiv.style.height = "0px";
   }
 }
 
@@ -29,9 +49,9 @@ function openIndex() {
 
 // Slowly scroll to bottom on tab click.
 function scrollToBottom() {
-  window.scroll({
-    top: document.body.scrollHeight - window.innerHeight,
-    left: 0,
-    behavior: 'smooth'
-  });
+  window.scrollBy(0,10);
+  if (window.pageYOffset < document.body.scrollHeight
+    - window.innerHeight) {
+    scrolldelay = setTimeout(scrollToBottom, 5);
+  }
 }
